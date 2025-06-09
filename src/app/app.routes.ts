99776250467a6d3a3d1fbdc79ea02042ede13e50
romyluna <134311514+romyluna@users.noreply.github.com>
2025-06-09 16:43:54 -0300

    angular: crecion carpeta bases y despues de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
+import { CounterPageComponent } from './pages/counter/counter-page.component';
 import { Routes } from '@angular/router';
+import { HeroPageComponent } from './pages/hero/hero-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CounterPageComponent,
+
+  },
+
+ {
+    path: 'hero',
+    component: HeroPageComponent,
+  }
+
+];
